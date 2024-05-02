3a24f498e112999f8189d4d8ec3b8c1eed132602
Lingeshwaransenthilkumar <131688450+Lingeshwaransenthilkumar@users.noreply.github.com>
2024-05-03 00:06:16 +0530

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const userController=require("../controllers/users");
+const routes=express.Router();
+//! these routes leads to inner loop routes like /../../
+routes.post("/register",userController.register);
+routes.post("/login",userController.login);
+routes.post("/forget",userController.forget);
+routes.get("/logout",userController.logout);
+module.exports=routes;
---
+++ routes/page.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const routes=express.Router();
+const userController=require('../controllers/users');
+//! organising routes
+
+routes.get(["/","/login"],(req,res)=>{
+    res.render("login");
+});
+routes.get("/register",(req,res)=>{
+  res.render("register");
+});
+routes.get("/profile",userController.isLoggedIn,(req,res)=>{
+  if(req.user){
+    res.render("profile",{user:req.user});
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+routes.get("/home",userController.isLoggedIn,(req,res)=>{
+  //console.log(req.name);
+  if (req.user){
+    res.render("home",{user:req.user});;
+  }
+  else{
+    res.redirect("/login");
+
+  }
+});
+routes.get("/change_pass",(req,res)=>{
+  res.render("forget")
+})
+
+
+//* exporting whole module
+module.exports=routes;
+//* if we export single method , we use 
+//*export default method_name;
